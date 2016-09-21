e63dd5d09ceb9bae031f881a62eabb1b4dc4b098
miggle <chownema@gmail.com>
2016-09-21 16:50:35 +1200

    Post working
    - On button click, post is sent to hardcoded URL of the Server

--- HighlySecureTexter/app/src/main/java/com/aut/android/highlysecuretexter/MainActivity.java
+++ HighlySecureTexter/app/src/main/java/com/aut/android/highlysecuretexter/MainActivity.java
@@ -1,15 +1,57 @@
 package com.aut.android.highlysecuretexter;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import com.loopj.android.http.*;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String responseString = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +61,62 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                final View v = view;
+                post();
+                new AsyncTask<Void, Void, Void>() {
+                    @Override
+                    protected Void doInBackground(Void... voids) {
+
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(Void aVoid) {
+                        super.onPostExecute(aVoid);
+                        Snackbar.make(v, "Response : " + responseString, Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+
+                    }
+
+
+                }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, null);
+
+
+            }
+        });
+    }
+
+    public void post()
+    {
+        AsyncHttpClient client = new AsyncHttpClient();
+        String URL = "http://156.62.62.37:8080/PKAServer/webresources/pka/request/012556332";
+        String URL2 = "http://172.28.41.238:8080/PKAServer/webresources/pka/request/012556332";
+        client.post(URL2, new AsyncHttpResponseHandler() {
+
+            @Override
+            public void onStart() {
+                // called before request is started
+                Toast.makeText(MainActivity.this, "ATTEMPTING", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                Toast.makeText(MainActivity.this, "SUCCESS", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Toast.makeText(MainActivity.this, "FAIL", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onRetry(int retryNo) {
+                // called when request is retried
             }
         });
     }
