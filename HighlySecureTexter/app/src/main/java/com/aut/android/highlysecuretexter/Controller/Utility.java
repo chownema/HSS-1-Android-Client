f26d6974e452dd9d629d925731985d9e109e86aa
miggle <chownema@gmail.com>
2016-09-28 19:03:55 +1300

    Refactored httphelper and clienthelper class into utility class

---
+++ HighlySecureTexter/app/src/main/java/com/aut/android/highlysecuretexter/Controller/Utility.java
@@ -0,0 +1,177 @@
+package com.aut.android.highlysecuretexter.Controller;
+
+import android.util.Base64;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
+
+import static javax.crypto.Cipher.ENCRYPT_MODE;
+
+
+/**
+ * Created by MI on 28/09/16.
+ */
+
+public class Utility {
+
+    public final static byte[] salt = {-84, 40, -10, -53, -80, 90, -57, 125};
+    public final static String endpoint = "http://172.28.56.205:8080/PKAServer/webresources/pka";
+
+    // Keys
+    public static SecretKey ephemeralKey = null;
+
+    public static PublicKey pkaPubKey = null;
+    public static PrivateKey privateKey = null;
+    public static PublicKey publicKey = null;
+
+
+    public static void init(String phoneNum) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        // get pka key
+        pkaPubKey = KeyFactory.getInstance("RSA").generatePublic
+                (new X509EncodedKeySpec(decodeFromBase64(doPost("request/"+phoneNum))));
+        // Check is not null
+        if (pkaPubKey != null) {
+            throw new RuntimeException("Key Error");
+        }
+
+
+
+    }
+
+    public static void generateEphemeral(String password) {
+
+        SecretKey key = null;
+
+        try {
+            char[] passwordChar = password.toCharArray();
+            PBEKeySpec pbeSpec = new PBEKeySpec(passwordChar, salt, 1000);
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
+            key = keyFactory.generateSecret(pbeSpec);
+        } catch (Exception ex) {
+            Log.e("Error", ex.toString());
+        }
+
+        ephemeralKey = key;
+    }
+
+    public static void generateKeys() {
+        try {
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+            kpg.initialize(2048);
+            KeyPair keyPair = kpg.generateKeyPair();
+            publicKey = keyPair.getPublic();
+            privateKey = keyPair.getPrivate();
+        } catch (NoSuchAlgorithmException ex) {
+            Log.e("Error", ex.toString());
+        }
+    }
+
+    private byte[] encryptConnectionData(String phoneNum) {
+
+        try {
+            // Prep cipher
+            Cipher pbeCipher = Cipher.getInstance("PBEWithMD5AndDES/CBC/PKCS5Padding");
+            pbeCipher.init(ENCRYPT_MODE, ephemeralKey, new PBEParameterSpec(salt, 1000));
+
+            // Encrypt nonce with pub key of pka (added security)
+            Cipher rsaCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+            rsaCipher.init(ENCRYPT_MODE, pkaPubKey);
+            byte[] nonceBytes = rsaCipher.doFinal(phoneNum.getBytes());
+
+            // Package data
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            baos.write(phoneNum.getBytes());
+            baos.write("---".getBytes());
+            baos.write(Base64.encode(nonceBytes, Base64.DEFAULT)); // encrypted with private RSA
+            baos.write("---".getBytes());
+            baos.write(Base64.encode(publicKey.getEncoded(), Base64.DEFAULT));
+
+            // Encrypt and return
+            byte[] cipherBytes = pbeCipher.doFinal(baos.toByteArray());
+            return cipherBytes;
+
+        } catch (Exception ex) {
+            Log.e("Error", ex.toString());
+        }
+        return null;
+    }
+
+
+
+
+
+
+    /**
+     * Sending Methods
+     */
+
+    public static byte[] decodeFromBase64(String cipher) {
+        // HTML decode from transport
+        cipher = cipher.replace("%2B", "+").replace("%2F", "/");
+        // Base64 decode
+        return Base64.decode(cipher, Base64.DEFAULT);
+    }
+
+    public static String encodeToBase64(byte[] data) {
+        // Encode bytes into base64
+        String encodedData = Base64.encodeToString(data, Base64.DEFAULT);
+        // HTML encode for transport
+        return encodedData.replace("+", "%2B").replace("/", "%2F");
+    }
+
+
+    public static String doPost(String restMethod) {
+
+        String data = null;
+
+        try {
+
+            URL url = new URL(endpoint + restMethod);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            if ((output = br.readLine()) != null) {
+                data = output;
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+
+}
--- HighlySecureTexter/app/src/main/java/com/aut/android/highlysecuretexter/LoginActivity.java
+++ HighlySecureTexter/app/src/main/java/com/aut/android/highlysecuretexter/LoginActivity.java
@@ -4,118 +4,68 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.aut.android.highlysecuretexter.Controller.ClientHelper;
-import com.aut.android.highlysecuretexter.Controller.HttpHelper;
-
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-
-import static android.R.attr.absListViewStyle;
 import static android.R.attr.value;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button requestEmailButton, connectButton;
-    EditText yourNumberEditText, passwordEditText;
-    private HttpHelper httpHelper;
+    // Screen elements
+    private Button requestConnectionButton, connectButton;
+    private EditText yourNumberEditText, passwordEditText;
 
-    ClientHelper client;
+    // Public Strings
+    public String response;
+    public String password;
+    public String number;
 
-    String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
         setContentView(R.layout.login_activity);
 
+        // Set Edit Text
         yourNumberEditText = (EditText) findViewById(R.id.phone_edit_text);
         passwordEditText = (EditText) findViewById(R.id.password_edit_text);
 
         // Set buttons
-        requestEmailButton = (Button) findViewById(R.id.email_request_button);
-        requestEmailButton.setOnClickListener(this);
+        requestConnectionButton = (Button) findViewById(R.id.email_request_button);
+        requestConnectionButton.setOnClickListener(this);
 
         connectButton = (Button) findViewById(R.id.connect_button);
         connectButton.setOnClickListener(this);
-
-        // Set up HTTP Helper
-        httpHelper = new HttpHelper(this);
-
-
     }
-
-    public void setPasswordText()
-    {
-        passwordEditText.setText(password);
-    }
-
-    public boolean canSetPasswordText(String pass)
-    {
-        if(pass!=null||pass!="") {
-            password = pass;
-            return false; // false to exit the loop
-        }
-        return true;
-    }
-
 
     @Override
     public void onClick(View view) {
-        // If Requesting email
-        if (view == requestEmailButton)
+        if (view == requestConnectionButton)
         {
-
-            String number = yourNumberEditText.getText().toString();
-
-            Toast.makeText(this, yourNumberEditText.getText().toString(), Toast.LENGTH_SHORT).show();
-            httpHelper.post("request/"+number);
+            // Capture Phonenumber
+            number = yourNumberEditText.getText().toString();
 
             // Set password in password field
             new AsyncTask<Void, Void, Void>() {
                 @Override
                 protected Void doInBackground(Void... voids) {
-                    while(canSetPasswordText(httpHelper.getResponse())) {
-                        //wait for response from server for password
-                    }
+                    response = "";
                     return null;
                 }
 
                 @Override
                 protected void onPostExecute(Void aVoid) {
                     // Set Password when received response
-                    String password = httpHelper.getResponse();
-                    setPasswordText();
-                    super.onPostExecute(aVoid);
+                    Log.e("Password Returned", response);
+                    Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
                 }
             }.execute();
-
-            // Encrypt and Store data
-            try {
-                client = new ClientHelper(yourNumberEditText.getText().toString(),
-                passwordEditText.getText().toString(), this);
-                // Set up Join Request
-                client.sendJoin();
-
-
-            } catch (InvalidKeySpecException e) {
-                e.printStackTrace();
-            } catch (NoSuchAlgorithmException e) {
-                e.printStackTrace();
-            }
-
-
         }
+
+        // Connect to the server with the encrypted cipher array
         if (view == connectButton)
         {
             Toast.makeText(getApplicationContext(), "Connecting...", Toast.LENGTH_SHORT).show();
@@ -125,5 +75,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             myIntent.putExtra("key", value); //Optional parameters
             LoginActivity.this.startActivity(myIntent);
         }
+    }
+
+    /**
+     * Setters
+     */
+
+    public void setPasswordText()
+    {
+        passwordEditText.setText(password);
     }
 }
